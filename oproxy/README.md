48676520ce8e86101903bdd6549bba923234b044
r2dev2 <ronak.badhe@gmail.com>
2024-07-08 01:07:13 -0700

    formatting setup

--- oproxy/README.md
+++ oproxy/README.md
@@ -6,8 +6,15 @@ Obuhersys using Proxies to track method calls.
 
 ```bash
 pnpm i # install dependencies
+pnpm run fmt # format code
 pnpm run dev # run src/index.ts file (for development)
 pnpm run build # compile the typescript to javascript in dist/
 pnpm run build:watch # auto-reload compiling the typescript to javascript in dist/
 pnpm run start # TODO this should run an example program with oproxy as a loader, not impled yet
 ```
+
+## Development
+
+This project uses [TypeScript](https://www.typescriptlang.org/) with [pnpm](https://pnpm.io/).
+
+[Semi-Standard](https://github.com/standard/semistandard) ([standard](https://github.com/standard/standard) with semicolons) formatting is used.
--- oproxy/src/index.ts
+++ oproxy/src/index.ts
@@ -1,13 +1,12 @@
-import * as crypto from "node:crypto"
-import { Cipher } from "node:crypto"
+import * as crypto from 'node:crypto';
+import { Cipher } from 'node:crypto';
 // import { FinalizationRegistry } from "node:util"
 
-
 // some random data
-const data: Buffer[] = [crypto.randomBytes(256)]
+const data: Buffer[] = [crypto.randomBytes(256)];
 
-const key = "potato".repeat(Math.ceil(32/6)).substring(0, 32)
-const iv = "beetroot"
+const key = 'potato'.repeat(Math.ceil(32 / 6)).substring(0, 32);
+const iv = 'beetroot';
 
 interface CipherObj {
   [finalCalledSymbol]?: boolean;
@@ -15,42 +14,42 @@ interface CipherObj {
 
 // Weakset of cipher objects created
 const createdCipherObjs: WeakRef<CipherObj>[] = [];
-const finalCalledSymbol = Symbol("[[finalCalled]]");
+const finalCalledSymbol = Symbol('[[finalCalled]]');
 
 const cipherHandler = {
-   //modify createCipheriv to return a proxy object that wraps the cipher object and tracks if final is called
-  apply: function (target : any, thisArg : any, argumentsList : any) { 
+  //modify createCipheriv to return a proxy object that wraps the cipher object and tracks if final is called
+  apply: function (target: any, thisArg: any, argumentsList: any) {
     console.log(`createCipheriv called with args: ${argumentsList}`);
-    
+
     const cipherObj = target(...argumentsList);
 
     // add the cipher object to the weakset
     createdCipherObjs.push(new WeakRef(cipherObj));
-    
+
     //wrap the cipherObj in another Proxy that keeps track of if final is ever called
     const cipherObjHandler = {
-      get: function(target: any, prop: any, receiver: any) {
-         // if calling final, set the finalCalledSymbol to true
-         if (prop === "final") {
-            target[finalCalledSymbol] = true;
-         }
+      get: function (target: any, prop: any, receiver: any) {
+        // if calling final, set the finalCalledSymbol to true
+        if (prop === 'final') {
+          target[finalCalledSymbol] = true;
+        }
         return Reflect.get(target, prop, receiver);
       },
-    }
-    
-   //create a proxy object that wraps the cipher object
+    };
+
+    //create a proxy object that wraps the cipher object
     const cipherProxy = new Proxy(cipherObj, cipherObjHandler);
-    
-    //return the cipher proxy      
+
+    //return the cipher proxy
     return cipherProxy;
   },
-}
+};
 
 // create a proxy for the createCipheriv function
 const createCipherivProxy = new Proxy(crypto.createCipheriv, cipherHandler);
 
 // create a cipher object that is actually our cipher proxy object
-const cipher: Cipher = createCipherivProxy("aes-256-gcm", key, iv)
+const cipher: Cipher = createCipherivProxy('aes-256-gcm', key, iv);
 
 // const registry = new FinalizationRegistry((heldValue) => {
 //   console.log(`Cleanup for object ${heldValue}`);
@@ -58,14 +57,14 @@ const cipher: Cipher = createCipherivProxy("aes-256-gcm", key, iv)
 
 // registry.register(cipher, 'cipher');
 
-const chunks: Buffer[] = []
+const chunks: Buffer[] = [];
 for (const chunk of data) {
-  chunks.push(cipher.update(chunk))
+  chunks.push(cipher.update(chunk));
 }
 
 const PULLTHELEVERKRONK = false;
 if (PULLTHELEVERKRONK) {
-  chunks.push(cipher.final()) // crucial line
+  chunks.push(cipher.final()); // crucial line
 }
 
 const decrypted: Buffer = Buffer.concat(chunks);
@@ -74,13 +73,12 @@ const decrypted: Buffer = Buffer.concat(chunks);
 process.on('exit', () => {
   for (const ref of createdCipherObjs) {
     let obj = ref.deref();
-    if (obj !== undefined && typeof obj === "object") {
+    if (obj !== undefined && typeof obj === 'object') {
       if (!obj![finalCalledSymbol]) {
-        console.log("Final was never called");
-        throw new Error("Final was never called");
-      }
-      else {
-        console.log("Final was called yay :))))");
+        console.log('Final was never called');
+        throw new Error('Final was never called');
+      } else {
+        console.log('Final was called yay :))))');
       }
     }
   }
@@ -93,7 +91,7 @@ process.on('exit', () => {
 // andrew awayyyyyy
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/FinalizationRegistry#notes_on_cleanup_callbacks
 // this is concerning though
-// halp, the concerns are real, clean ups 
+// halp, the concerns are real, clean ups
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WeakMap
 
