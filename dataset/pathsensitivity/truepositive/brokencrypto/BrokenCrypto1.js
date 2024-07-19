9f940e187e517d0fc67505947dafdcdb94d9f08e
cdevadhar <cdevadhar@gmail.com>
2024-07-18 18:37:12 -0700

    pathsensitivity (depth 1)

---
+++ dataset/pathsensitivity/truepositive/brokencrypto/BrokenCrypto1.js
@@ -0,0 +1,23 @@
+const crypto = require('crypto');
+async function main() {
+    try {
+        const condition = 0;
+        let cipher;
+        if (condition<1) {
+            //insecure algo
+            cipher = crypto.createCipheriv('des', crypto.randomBytes(8), crypto.randomBytes(8));
+            
+        }
+        else {
+            // secure algo
+            cipher = crypto.createCipheriv('aes-256-gcm', crypto.randomBytes(8), crypto.randomBytes(8));
+        }
+        const encrypted = cipher.update('some plaintext data', 'utf8', 'hex') + cipher.final('hex');
+        console.log("Encrypted: ", encrypted);
+    }
+    catch(err) {
+        console.log("Error: ", err.message);
+    }
+}
+
+main();
---
+++ dataset/pathsensitivity/truepositive/pbkdf2Parameters/pbkdf2Parameters1.js
@@ -0,0 +1,21 @@
+const crypto = require('crypto');
+
+const iterations = 10000000;
+const password = 'your-password'; 
+const keyLength = 32; 
+const digest = 'sha256';
+const condition  = 0;
+let salt;
+
+if (condition>1) {
+    salt = crypto.randomBytes(16);
+}
+else {
+    // static salt is insecure
+    salt = Buffer.from([123]); 
+}
+
+crypto.pbkdf2(password, salt, iterations, keyLength, digest, (err, derivedKey) => {
+    if (err) throw err;
+    console.log('Derived key:', derivedKey.toString('hex'));
+});
