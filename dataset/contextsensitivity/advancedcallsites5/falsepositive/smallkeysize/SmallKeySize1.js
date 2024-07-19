5dc8f06dd9927969a06b1498d5e06bf4c08e015d
Hunter Kang <hunterkang5@g.ucla.edu>
2024-07-18 17:47:21 -0700

    added yml files and Identity functions to contextsensitivity/advancedcallsite

--- dataset/contextsensitivity/advancedcallsites5/falsepositive/smallkeysize/SmallKeySize1.js
+++ dataset/contextsensitivity/advancedcallsites5/falsepositive/smallkeysize/SmallKeySize1.js
@@ -4,11 +4,11 @@ const crypto = require('crypto');
 // Five different keysize variables are stored in variables. One variable with a sufficient keysize is then used to generate a KeyPair.
 async function main() {
     try {
-        const object1 = { modulusLength: 1024 };
-        const object2 = { modulusLength: 3072 };
-        const object3 = { modulusLength: 128 };
-        const object4 = { modulusLength: 256 };
-        const object5 = { modulusLength: 512 };
+        const object1 = Identity({ modulusLength: 1024 });
+        const object2 = Identity({ modulusLength: 3072 });
+        const object3 = Identity({ modulusLength: 128 });
+        const object4 = Identity({ modulusLength: 256 });
+        const object5 = Identity({ modulusLength: 512 });
         const buffer = Buffer.from("whatever");
         const { publicKey, privateKey } = await util.promisify(crypto.generateKeyPair)("rsa", object2);
         const public = crypto.publicEncrypt(publicKey, buffer);
@@ -20,6 +20,10 @@ async function main() {
     } catch (error) {
         console.error('Error:', error.message);
     }   
+}
+
+function Identity(s) {
+    return s;
 }
 
 main();
