cecaa01dbcbbc7fdbb9009d560d487be902d3995
akellasqrt16 <64755067+akellasqrt16@users.noreply.github.com>
2024-07-25 19:07:41 -0700

    Current progress towards logging all method calls for ofunc.

--- ofunc/src/index.ts
+++ ofunc/src/index.ts
@@ -26,65 +26,26 @@ type GlobalPreloadHook = () => string;
 
 /**
  * Instruments a call expression with a call to log() to record method and arguments.
- *
- * So far, this will only work on x.y(a1, a2, ...).
- * TODO We need to come up with a solution for tracking:
- *  const fn = x.y;
- *  fn(a1, a2);
- * See footnote.
- *
- * Example:
- *
- * crypto.randomBytes(8)
- *
- * will be transformed to
- *
- * (() => {
- *   var args = [8];
- *   log(crypto, "randomBytes", args);
- *   return crypto.randomBytes(...args);
- * })();
- *
- * Breakdown:
- *  1. wrap in an anonymous lambda expression;
- *     this creates a function with the body inside of { } and then
- *     immediately calls it;
- *     we do this because the transformed code is still a CallExpression
- *     so we don't have to worry about constructing an incorrect syntax tree
- *  2. store the arguments into an array to avoid re-evaluation.
- *  3. call log with the object, its method name, and the args array
- *     (we log the object because we want to track order of methods called on object)
- *  4. perform the original call using the saved args and then return the result
- *
- * Footnote:
- * Perhaps to support a raw function call, we could instrument that code to
- *  const fn = propAccess(x, x.y);
- *  (() => {
- *    var args = [a1, a2];
- *    logFn(fn, args);
- *    return fn(...args);
- *  })();
- * And then propAccess can set y[Symbol('[[obu]]')] = { obj: x, meth: 'y' }.
- * And then logFn reads the secret key to find the object and method.
  */
-const addLog = (callexpr: ts.CallExpression) => {
-  if (ts.isPropertyAccessExpression(callexpr.expression)) {
-    return __addLog(
-      [...callexpr.arguments],
-      callexpr.expression.expression,
-      callexpr.expression.name,
-      callexpr
-    );
+const addLog = (callexpr: ts.CallExpression, obj: ts.Expression | undefined, methodName: string | undefined) => {
+  if (!methodName && ts.isPropertyAccessExpression(callexpr.expression)) {
+    methodName = callexpr.expression.name.getText();
+    obj = callexpr.expression.expression;
   }
-  return callexpr;
+
+  return __addLog(
+    [...callexpr.arguments],
+    obj,
+    methodName,
+    callexpr
+  );
 };
 
-// below tree acquired from
-// https://ts-ast-viewer.com/#code/BTCUAIF4D5wb3AYwPYDsDOAXcBDATgObpTgDaAHALoDc4ANsgcIngJ4AOmyANOAOR4cqACbIAtgCFWmAKbo+vfEVC08MzAFc8qJG07IAdIJHips9MANWl6FeAC+oMNSA
+// Instrument direct call expressions
 const __addLog = (
   args: ts.Expression[],
-  obj: ts.Expression,
-  prop: ts.Node,
+  obj: ts.Expression | undefined,
+  methodName: string | undefined,
   callexpr: ts.CallExpression
 ) =>
   factory.createCallExpression(
@@ -98,9 +59,6 @@ const __addLog = (
         factory.createBlock(
           [
             factory.createVariableStatement(
-              // empty means just var
-              // factory.createToken(SyntaxKind.ConstKeyword) will create a const
-              // however if that is supplied, we end up with a `const var` which is wrong
               [],
               factory.createVariableDeclarationList(
                 [
@@ -109,31 +67,24 @@ const __addLog = (
                     undefined,
                     undefined,
                     factory.createArrayLiteralExpression(
-                      // [factory.createNumericLiteral("8")],
                       args,
                       false
                     )
                   ),
                 ]
-                // ts.NodeFlags.Const | ts.NodeFlags.Constant | ts.NodeFlags.Constant
-                // above argument was outputted by the ts-ast site, however
-                // above is not supported in new typescript version, use first argument
-                // of ModifierLike[] to change type of var
               )
             ),
-            factory.createExpressionStatement(
+            methodName && obj ? factory.createExpressionStatement(
               factory.createCallExpression(
                 factory.createIdentifier('$_obu_log'),
                 undefined,
                 [
-                  // factory.createIdentifier("crypto"),
                   obj,
-                  // factory.createStringLiteral("randomBytes"),
-                  factory.createStringLiteral(prop.getText()),
+                  factory.createStringLiteral(methodName),
                   factory.createIdentifier('$_args'),
                 ]
               )
-            ),
+            ) : undefined,
             factory.createReturnStatement(
               factory.updateCallExpression(
                 callexpr,
@@ -146,7 +97,7 @@ const __addLog = (
                 ]
               )
             ),
-          ],
+          ].filter(node => node !== undefined) as ts.Statement[],
           false
         )
       )
@@ -155,24 +106,143 @@ const __addLog = (
     []
   );
 
-function transformSourceFile(sourceFile: ts.SourceFile) {
-  function visitor(node: ts.Node) {
-    if (ts.isCallExpression(node)) {
-      // only instrument function calls
-      return [addLog(node)];
-    }
-    return ts.visitEachChild(node, visitor, undefined);
+// Instrument variable assignments with method references
+const instrumentAssignment = (assignment: ts.BinaryExpression) => {
+  if (ts.isPropertyAccessExpression(assignment.right)) {
+    const methodName = assignment.right.name.getText();
+    const obj = assignment.right.expression;
+
+    const symbolKey = factory.createCallExpression(
+        factory.createPropertyAccessExpression(
+          factory.createIdentifier('Symbol'),
+          'for'
+        ),
+        undefined,
+        [factory.createStringLiteral('[[obu]]')]
+    );
+
+    const methodObject = factory.createObjectLiteralExpression(
+      [
+        factory.createPropertyAssignment('obj', obj),
+        factory.createPropertyAssignment('method', factory.createStringLiteral(methodName))
+      ]
+    );
+
+    const newRight = factory.createBinaryExpression(
+      assignment.right,
+      factory.createToken(ts.SyntaxKind.EqualsToken),
+      factory.createAssignment(
+        factory.createElementAccessExpression(
+          assignment.right,
+          symbolKey
+        ),
+        methodObject
+      )
+    );
+
+    return factory.createBinaryExpression(
+      assignment.left,
+      assignment.operatorToken,
+      newRight
+    );
   }
 
-  const context = {
-    factory: ts.factory,
-  };
+  return assignment;
+};
+
+function transformSourceFile(sourceFile: ts.SourceFile) {
+  function visitor(node: ts.Node): ts.Node {
+    if (ts.isBinaryExpression(node) && node.operatorToken.kind === ts.SyntaxKind.EqualsToken) {
+      // Instrument assignments
+      return instrumentAssignment(node);
+    } else if (ts.isCallExpression(node)) {
+      // Instrument direct method calls and calls on variables
+      const expr = node.expression;
+      if (ts.isIdentifier(expr)) {
+        const symbolKey = factory.createCallExpression(
+            factory.createPropertyAccessExpression(
+              factory.createIdentifier('Symbol'),
+              'for'
+            ),
+            undefined,
+            [factory.createStringLiteral('[[obu]]')]
+        );
+
+        const contextVar = factory.createVariableDeclaration(
+          factory.createIdentifier('$_context'),
+          undefined,
+          undefined,
+          factory.createLogicalAnd(
+            factory.createIdentifier(expr.getText()),
+            factory.createElementAccessExpression(
+              factory.createIdentifier(expr.getText()),
+              symbolKey
+            )
+          )
+        );
+
+        const logCall = factory.createExpressionStatement(
+          factory.createCallExpression(
+            factory.createIdentifier('$_obu_log'),
+            undefined,
+            [
+              factory.createElementAccessExpression(factory.createIdentifier('$_context'), factory.createStringLiteral('obj')),
+              factory.createElementAccessExpression(factory.createIdentifier('$_context'), factory.createStringLiteral('method')),
+              factory.createIdentifier('$_args')
+            ]
+          )
+        );
+
+        const argsVar = factory.createVariableDeclaration(
+          factory.createIdentifier('$_args'),
+          undefined,
+          undefined,
+          factory.createArrayLiteralExpression([...node.arguments])
+        );
+
+        const updatedNode = factory.createBlock(
+          [
+            factory.createVariableStatement([], factory.createVariableDeclarationList([contextVar, argsVar], ts.NodeFlags.Const)),
+            factory.createIfStatement(
+              factory.createIdentifier('$_context'),
+              logCall
+            ),
+            factory.createReturnStatement(factory.updateCallExpression(
+              node,
+              node.expression,
+              node.typeArguments,
+              [factory.createSpreadElement(factory.createIdentifier('$_args'))]
+            ))
+          ],
+          true
+        );
+
+        return factory.createCallExpression(
+          factory.createParenthesizedExpression(
+            factory.createArrowFunction(
+              undefined,
+              undefined,
+              [],
+              undefined,
+              factory.createToken(ts.SyntaxKind.EqualsGreaterThanToken),
+              updatedNode
+            )
+          ),
+          undefined,
+          []
+        );
+      } else {
+        return addLog(node, undefined, undefined);
+      }
+    }
+
+    return ts.visitEachChild(node, visitor, undefined);
+  }
 
   return ts.visitNode(sourceFile, visitor);
 }
 
 const instrumentSource = (src: string): string => {
-  // parse src into typescript, name file temp.ts
   const sourceFile = ts.createSourceFile(
     'temp.ts',
     src,
@@ -180,25 +250,27 @@ const instrumentSource = (src: string): string => {
     true
   );
 
-  // add calls to log to each function call
   const result = transformSourceFile(sourceFile);
   if (result === undefined) {
     return src;
   }
 
-  // convert our modified syntax tree to javascript sourcecode
   const printer = ts.createPrinter();
   const newCode = printer.printNode(
     ts.EmitHint.Unspecified,
     result,
     sourceFile
   );
-  return newCode;
+
+  const logFunction = `
+    function $_obu_log(obj, method, args) {
+      console.log('method:', method, 'args', args);
+    }
+  `;
+
+  return logFunction + '\n' + newCode;
 };
 
-/**
- * This hook decides what code to load for a javascript file.
- */
 export const load: LoadHook = async function (url, context, nextLoad) {
   if (process.env.DEBUG) {
     console.log(`Loading URL: ${url}`);
@@ -215,9 +287,6 @@ export const load: LoadHook = async function (url, context, nextLoad) {
       const sourceCode = r.source.toString();
 
       const newSource = instrumentSource(sourceCode);
-      // run
-      // export DEBUG=yes
-      // before running this script to see this output
       if (process.env.DEBUG) {
         console.log('start src---');
         console.log(newSource);
@@ -235,9 +304,6 @@ export const load: LoadHook = async function (url, context, nextLoad) {
   return r;
 };
 
-// this is code to prepare the globalThis object
-// i don't think common functions like require exist at
-// the stage this code runs so it's probably not worth
 export const globalPreload: GlobalPreloadHook = function () {
   return `
     globalThis['$obu_log'] = (obj, methodname, args) => {
