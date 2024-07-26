0ae0233e53e7ca335050730f223afaf868fea1b2
Leon Liu <liuleon360@gmail.com>
2024-07-25 19:06:04 -0700

    Add checks
    Co-authored-by: Andrew <Arc-blroth@users.noreply.github.com>

--- oproxy/src/tests/checks.js
+++ oproxy/src/tests/checks.js
@@ -1,35 +1,120 @@
 [
   {
-    "name": "call .final()",
-    "trigger": {
-      "type": "constructor",
-      "lib": "crypto",
-      "fn": "createDecipheriv",
-      "args": ["aes-256-gcm"]
+    name: "initial motivating example - authTag without .final() vuln",
+    trigger: {
+      type: "constructor",
+      lib: "crypto",
+      fn: "createDecipheriv",
+      args: ["aes-256-gcm"]
     },
-    "implies": [
+    implies: [
       {
-        "type": "mustCall",
-        "method": "final",
-        "implies": [{
-          "type": "mustNotCall",
-          "method": ".*" 
+        type: "mustCall",
+        method: "final",
+        implies: [{
+          type: "mustNotCall",
+          method: ".*" 
         }]
       }
     ]
   },
   {
-    "name": "no insecure random",
-    "trigger": {
-      "type": "constructor",
-      "lib": "crypto",
-      "fn": "createEncipheriv"
+    name: "basic/brokenCrypto",
+    trigger: {
+      type: "constructor",
+      lib: "crypto",
+      fn: "createEncipheriv"
     },
-    "implies": [
+    implies: [
       {
-        "type": "argMustNotMatch",
-        "index": 0,
-        "arg": ["des|rc4|rc2|blowfish", "i"]
+        type: "validateArg",
+        index: 0,
+        validate: arg => !/des|rc-?[245]|blowfish/i.test(arg)
+      }
+    ]
+  },
+  {
+    name: "basic/brokenHash",
+    trigger: {
+      type: "constructor",
+      lib: "crypto",
+      fn: "createHash"
+    },
+    implies: [
+      {
+        type: "validateArg",
+        index: 0,
+        validate: arg => !/md5|sha1/i.test(arg)
+      }
+    ]
+  },
+  {
+    name: "basic/ecbMode",
+    trigger: {
+      type: "constructor",
+      lib: "crypto",
+      fn: "createCipheriv"
+    },
+    implies: [
+      {
+        type: "validateArg",
+        index: 0,
+        validate: arg => arg.toLowerCase() !== "aes-256-ecb"
+      }
+    ]
+  },
+  {
+    name: "basic/pdkdf2Paramters",
+    trigger: {
+      type: "constructor",
+      lib: "crypto",
+      fn: "pbkdf2"
+    },
+    implies: [
+      // Limitation unable to check if salt is static or random, static salt is insecure
+      {
+        // too small numbers of iterations should be at least 10,000
+        type: "validateArg",
+        index: 2,
+        validate: arg => arg >= 10000
+      },
+      {
+        // 'sha-1' insecure hashing algorithm
+        type: "validateArg",
+        index: 4,
+        validate: arg => !/md5|sha1/i.test(arg)
+      }
+    ]
+  },
+  {
+    name: "basic/smallkeysize",
+    trigger: {
+      type: "constructor",
+      lib: "crypto",
+      fn: "generateKeyPair",
+      args: ["rsa"]
+    },
+    implies: [
+      {
+        type: "validateArg",
+        index: 1,
+        validate: arg => arg >= 2048
+      }
+    ]
+  },
+  {
+    name: "basic/staticIV",
+    trigger: {
+      type: "constructor",
+      lib: "crypto",
+      fn: "generateKeyPair",
+      args: ["rsa"]
+    },
+    implies: [
+      {
+        type: "validateArg",
+        index: 1,
+        validate: arg => arg >= 2048
       }
     ]
   }
