d4f201f896250370fe44d39a1d17ccb42a7f9401
r2dev2 <ronak.badhe@gmail.com>
2024-08-05 19:53:54 -0700

    oproxy: bring back ast mods fixing namespace import

--- oproxy/src/hooks.ts
+++ oproxy/src/hooks.ts
@@ -1,3 +1,8 @@
+import { Comment, Node as AcornNode, Parser, Program as AcornProgram } from 'acorn';
+import * as walk from 'acorn-walk';
+import { Program } from 'estree';
+import { attachComments } from 'estree-util-attach-comments';
+import { toJs } from 'estree-util-to-js';
 import path from 'node:path';
 import fs from 'node:fs';
 import { SourceMapGenerator } from 'source-map';
@@ -59,7 +64,66 @@ export async function load(
     // Don't modify if it's the proxy module importing the original module, avoid circular import
     // console.log(`loading proxy ${url}\n`);
     r.format = 'commonjs';
+    return r;
   }
+
+  if (
+    r.source instanceof Int8Array ||
+    r.source instanceof Uint8Array ||
+    r.source instanceof Uint8ClampedArray ||
+    r.source instanceof Int16Array ||
+    r.source instanceof Uint16Array ||
+    r.source instanceof Int32Array ||
+    r.source instanceof Uint32Array ||
+    r.source instanceof Float32Array ||
+    r.source instanceof Float64Array ||
+    r.source instanceof BigInt64Array ||
+    r.source instanceof BigUint64Array
+  ) {
+    r.source = r.source.buffer;
+  }
+
+  if (r.source instanceof ArrayBuffer) {
+    // force r.source to be a string
+    r.source = new TextDecoder().decode(r.source);
+  }
+
+  /**
+   * The source might have an import * as x from 'module'.
+   *
+   * This runs into commonjs/esm compatibility issues resulting in x not being
+   * the desired module.
+   *
+   * We fix such namespace imports.
+   */
+  const comments: Comment[] = [];
+  const ast = Parser.parse(r.source, {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    locations: true,
+    onComment: comments,
+  });
+  attachComments(ast as Program, comments);
+
+  const specifierFixupVisitors: walk.SimpleVisitors<string[]> = {
+    ImportNamespaceSpecifier(node, state) {
+      state.push(node.local.name);
+      node.local.name += UNWRAP_NAMESPACE_PLS;
+    },
+  };
+
+  walk.simple(ast, {
+    ImportDeclaration(node) {
+      const toUnwrap: string[] = [];
+      walk.simple(node, specifierFixupVisitors, undefined, toUnwrap);
+      unwrapNamespaceImports(ast, node, toUnwrap);
+    },
+  });
+
+  const js = toJs(ast as Program, { filePath: url, SourceMapGenerator });
+
+  r.source = js.value + '\n//# sourceMappingURL=data:application/json;base64,' + btoa(JSON.stringify(js.map));
+
   return r;
 }
 
@@ -71,4 +135,49 @@ function pathurlResolveProxy(module: string): string {
 function resolveProxy(module: string): string {
   const fileurl = pathToFileURL(path.join(path.dirname(fileURLToPath(import.meta.url)), 'proxymodules', module)).toString();
   return fileurl.slice(7);
+}
+
+function unwrapNamespaceImports(ast: AcornProgram, after: AcornNode, toUnwrap: string[]) {
+  const source = {
+    start: after.start,
+    end: after.end,
+    range: after.range,
+    loc: after.loc,
+  };
+  const index = ast.body.findIndex((x) => Object.is(x, after)) + 1;
+  // console.log('inserting stuff at', index);
+  toUnwrap.forEach((unwrapped) => {
+    ast.body.splice(index, 0, {
+      type: 'VariableDeclaration',
+      kind: 'const',
+      declarations: [
+        {
+          type: 'VariableDeclarator',
+          id: {
+            type: 'Identifier',
+            name: unwrapped,
+            ...source,
+          },
+          init: {
+            type: 'MemberExpression',
+            object: {
+              type: 'Identifier',
+              name: unwrapped + UNWRAP_NAMESPACE_PLS,
+              ...source,
+            },
+            property: {
+              type: 'Identifier',
+              name: 'default',
+              ...source,
+            },
+            computed: false,
+            optional: false,
+            ...source,
+          },
+          ...source,
+        },
+      ],
+      ...source,
+    });
+  });
 }
